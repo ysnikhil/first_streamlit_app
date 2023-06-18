062d1d255e736022012b593f9d78aceffa1f7d0d
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-18 14:08:41 +0530

    Update streamlit_app.py
    Make the Fruit column as the index and add multipicker for that index.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,16 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+#Read the S3 file given by snowflake in pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Make the first column as the index so that multipicker can read the data well
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
+#Add multipicker on the created index
 streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+
+#Load the dataframe in streamlit app
 streamlit.dataframe(my_fruit_list)
+
+
