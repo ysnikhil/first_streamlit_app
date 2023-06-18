c73d056475bcc4394c7cb2437e0c00a87193034c
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-18 13:26:31 +0530

    Update streamlit_app.py
    Added pandas package to import a library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+import pandas
 
+"""Building first streamlit app using python on HEALTHY DINER"""
+
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -10,3 +13,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
