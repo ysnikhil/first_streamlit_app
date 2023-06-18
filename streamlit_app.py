50f8d8bf35cfd2179cd0463627d5737a2cf26a16
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-18 14:14:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
 #Add multipicker on the created index
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 #Load the dataframe in streamlit app
 streamlit.dataframe(my_fruit_list)
