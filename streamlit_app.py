082c22bc4976795ea89d2241c31aac1b75a3e6bd
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-25 00:02:45 +0530

    Update streamlit_app.py
    Convert api response to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to displace the fruityvice fruits advice
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
 
 
 
