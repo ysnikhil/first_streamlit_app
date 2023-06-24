2e75b62f62447e00f8f3b83fdbfca016c552b027
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-25 00:09:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 #New section to displace the fruityvice fruits advice
+streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 normalized_api_json_response=pandas.json_normalize(fruityvice_response.json())
 
