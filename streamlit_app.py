6a84caf6401cb9312055c000a842b49e78e5f9d5
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-25 18:28:30 +0530

    Update streamlit_app.py
    Fetch all the records from the snowflake fruits table

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,10 +52,10 @@ my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 #my_cur.execute("select current_user(), current_account(), current_region()")
 my_cur.execute("select * from fruit_load_list")
-my_data_row=my_cur.fetchone()
+my_data_rows=my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.text("The Fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
