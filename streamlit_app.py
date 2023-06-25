8705c49be9491b66a7aa2ab36f5cfdcae03d85f8
Nikhil YS <nikhil.nyk007@gmail.com>
2023-06-25 18:19:00 +0530

    Update streamlit_app.py
    Query snowflake fruit load list table

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,11 @@ streamlit.dataframe(normalized_api_json_response)
 
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(), current_region()")
+#my_cur.execute("select current_user(), current_account(), current_region()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row=my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")
+streamlit.text("The Fruit load list contains:")
 streamlit.text(my_data_row)
 
 
